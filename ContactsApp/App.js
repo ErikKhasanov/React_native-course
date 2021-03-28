09b45d4d6dcc3a74f95b67e03c97f41a985e59d2
hasanov.e <hasanov.e@mxm.email>
2021-03-28 11:07:59 +0300

    Добавил проект, сделал первое дз

---
+++ ContactsApp/App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import ContactCard from './components/ContactCard'
+
+import {
+	View,
+	StyleSheet
+} from 'react-native';
+
+const styles = StyleSheet.create({
+	root: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+})
+
+const cardData = [
+	{
+		name: 'Laenne Graham',
+		phone: '010-692-6593 * 09125',
+		address: '53198-4257, South Elvis, Hoeger Mall, Apt. 692',
+		mail: 'graham@yahoo.com',
+		uri: 'https://reactnative.dev/img/tiny_logo.png',
+	},
+	{
+		name: 'Laenne Graham',
+		phone: '010-692-6593 * 09125',
+		address: '53198-4257, South Elvis, Hoeger Mall, Apt. 692',
+		mail: 'graham@yahoo.com',
+		uri: 'https://reactnative.dev/img/tiny_logo.png',
+	},
+	{
+		name: 'Laenne Graham',
+		phone: '010-692-6593 * 09125',
+		address: '53198-4257, South Elvis, Hoeger Mall, Apt. 692',
+		mail: 'graham@yahoo.com',
+		uri: 'https://reactnative.dev/img/tiny_logo.png',
+	},
+	{
+		name: 'Laenne Graham',
+		phone: '010-692-6593 * 09125',
+		address: '53198-4257, South Elvis, Hoeger Mall, Apt. 692',
+		mail: 'graham@yahoo.com',
+		uri: 'https://reactnative.dev/img/tiny_logo.png',
+	},
+	{
+		name: 'Laenne Graham',
+		phone: '010-692-6593 * 09125',
+		address: '53198-4257, South Elvis, Hoeger Mall, Apt. 692',
+		mail: 'graham@yahoo.com',
+		uri: 'https://reactnative.dev/img/tiny_logo.png',
+	},
+]
+
+
+const App = () => {
+	const renderCards = () => {
+	 return cardData.map((item, index) => (
+			<ContactCard
+				name={item.name}
+				phone={item.phone}
+				address={item.address}
+				mail={item.mail}
+				uri={item.uri}
+				key={index}
+			/>
+		))
+	}
+	return (
+		<View style={styles.root}>
+			{renderCards()}
+		</View>
+	);
+};
+
+export default App;
---
+++ ContactsApp/components/ContactCard/index.js
@@ -0,0 +1,102 @@
+import React, {useState, useRef} from 'react';
+
+import {
+	TouchableOpacity,
+	StyleSheet,
+	Text,
+	View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+	cardStyle: {
+		flexDirection: 'row',
+		minWidth: '95%',
+		backgroundColor: '#E5E5E5',
+		borderRadius: 10,
+		paddingTop: 20,
+		paddingBottom: 20,
+		paddingLeft: 30,
+		paddingRight: 30,
+		marginVertical: 10
+	},
+	avatarWrapper: {
+		width: '30%'
+	},
+	contentWrapper: {
+		width: '70%'
+	},
+	nameStyle: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		color: '#00ADD3'
+	},
+	iconStyle: {
+		backgroundColor: 'red',
+		height: 64,
+		width: 64,
+		borderRadius: 30
+	},
+	phoneStyle: {
+		fontSize: 16,
+		fontWeight: '200',
+		color: '#00ADD3'
+	},
+	contactWrapper: {
+		maxHeight: 0,
+	},
+	contactWrapper__open: {
+		paddingTop: 25,
+		maxHeight: 160
+	},
+	contactItem: {
+		// margin: '10, 0'
+	}
+});
+
+const App = ({name, phone, address, mail, uri}) => {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const toggleCard = () => {
+		setIsOpen(!isOpen);
+	}
+
+	return (
+		<TouchableOpacity onPress={toggleCard}>
+			<View style={styles.cardStyle}>
+				<View style={styles.avatarWrapper}>
+					<View style={styles.iconStyle}/>
+				</View>
+				<View style={styles.contentWrapper}>
+					<View style={styles.titleBox}>
+						<Text style={styles.nameStyle}>
+							{name}
+						</Text>
+						<Text style={styles.phoneStyle}>
+							{phone}
+						</Text>
+					</View>
+					<View style={[styles.contactWrapper, isOpen && styles.contactWrapper__open]}>
+						<View style={styles.contactItem}>
+							<Text style={styles.infoTitle}>
+								ADDRESS
+							</Text>
+							<Text style={styles.phoneStyle}>
+								{address}
+							</Text>
+						</View>
+						<View style={styles.contactItem}>
+							<Text style={styles.infoTitle}>
+								MAIL
+							</Text>
+							<Text style={styles.phoneStyle}>
+								{mail}
+							</Text>
+						</View>
+					</View>
+				</View>
+			</View>
+		</TouchableOpacity>
+	);
+};
+
+export default App;
